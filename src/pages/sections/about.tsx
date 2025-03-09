cf1b85b901c15c48381b9fdcc79f58cbd1c5ddac
UTTEJ KURUMA <uttej09092001@gmail.com>
2025-03-09 13:05:57 +0530

    Updated swiper dependencies

--- src/pages/sections/about.tsx
+++ src/pages/sections/about.tsx
@@ -1,6 +1,3 @@
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
