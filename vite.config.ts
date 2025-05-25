2ce172a211d3389127286d81fb27f57a3cd4c076
UTTEJ KURUMA <uttej09092001@gmail.com>
2025-05-25 19:08:41 +0530

    Added base to the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "https://alohatmc.com",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
